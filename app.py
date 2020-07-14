a90d3b88bbfeb730c935a15828e7a7f4ad361656
Rob <rlau929@gmail.com>
2020-07-14 12:10:06 -0700

    Adding app.py file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_pymongo import flask_pymongo
+from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
@@ -15,10 +15,10 @@ def index():
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_date = scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return "Scraping Successful!"
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
 
 if __name__ == "__main__":
     app.run()
