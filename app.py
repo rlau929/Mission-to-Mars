69dc9512e708efe8ff51016195c96e1a1864ff0a
Rob <rlau929@gmail.com>
2020-07-19 04:02:20 -0700

    Updating app file for Challenge

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scraping
+import challenge
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/challenge_app"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template("challenge_index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = challenge.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
